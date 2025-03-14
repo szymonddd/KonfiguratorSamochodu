9fd2ae18bea0771192b42de4fa80867182acf6a4
szymond <memememe@gmail.com>
2025-03-14 14:27:32 +0100

    TextView color + zmienne commit

--- app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
+++ app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.konfiguratorsamochodu
 
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
+    private lateinit var radioGroupCarModel: RadioGroup
+    private lateinit var radioSedan: RadioButton
+    private lateinit var radioSUV: RadioButton
+    private lateinit var radioHatchback: RadioButton
+    private lateinit var checkboxAC: CheckBox
+    private lateinit var checkboxLeatherSeats: CheckBox
+    private lateinit var imageViewSedan: ImageView
+    private lateinit var imageViewSUV: ImageView
+    private lateinit var imageViewHatchback: ImageView
+    private lateinit var textViewSummary: TextView
