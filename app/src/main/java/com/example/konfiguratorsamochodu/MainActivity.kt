0f8a0f4ff2fd93cee76de08604e34ba9c27d368a
Szymon D <szymon.dabrowski@cyfroweszkoly.pl>
2025-03-19 22:27:10 +0100

    update function

--- app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
+++ app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.konfiguratorsamochodu
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.RadioButton
@@ -13,13 +15,79 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var radioGroupCarModel: RadioGroup
-    private lateinit var radioSedan: RadioButton
-    private lateinit var radioSUV: RadioButton
-    private lateinit var radioHatchback: RadioButton
-    private lateinit var checkboxAC: CheckBox
-    private lateinit var checkboxLeatherSeats: CheckBox
-    private lateinit var imageViewSedan: ImageView
-    private lateinit var imageViewSUV: ImageView
-    private lateinit var imageViewHatchback: ImageView
-    private lateinit var textViewSummary: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val autosedan = findViewById<RadioButton>(R.id.sedan)
+        val autosuv = findViewById<RadioButton>(R.id.suv)
+        val autohatchback = findViewById<RadioButton>(R.id.hatchback)
+        val przycisk = findViewById<Button>(R.id.button_submit)
+        val wynik = findViewById<TextView>(R.id.textViewPodsumowanie)
+        val zdj = findViewById<ImageView>(R.id.imageViewzdj)
+        val klimatyzacja = findViewById<CheckBox>(R.id.klima)
+        val skorzanesiedzenia = findViewById<CheckBox>(R.id.siedzenie)
+
+        autosedan.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                zdj.setImageResource(R.drawable.sedan)
+            }
+        }
+
+        autosuv.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                zdj.setImageResource(R.drawable.suv)
+            }
+        }
+
+        autohatchback.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                zdj.setImageResource(R.drawable.hatchback)
+            }
+        }
+
+        przycisk.setOnClickListener {
+            if (klimatyzacja.isChecked && skorzanesiedzenia.isChecked) {
+                if (autosedan.isChecked) {
+                    wynik.setText("Twoja konfiguracja: Sedan +klimatyzacja +skórzane siedzenia")
+                    wynik.setBackgroundColor(Color.RED)
+                } else if (autosuv.isChecked) {
+                    wynik.setText("Twoja konfiguracja: SUV +klimatyzacja +skórzane siedzenia")
+                    wynik.setBackgroundColor(Color.RED)
+                } else if (autohatchback.isChecked) {
+                    wynik.setText("Twoja konfiguracja: Hatchback +klimatyzacja +skórzane siedzenia")
+                    wynik.setBackgroundColor(Color.RED)
+                }
+            }
+            if (klimatyzacja.isChecked && !skorzanesiedzenia.isChecked)
+                if (autosedan.isChecked) {
+                    wynik.setText("Twoja konfiguracja: Sedan +klimatyzacja")
+                } else if (autosuv.isChecked) {
+                    wynik.setText("Twoja konfiguracja: SUV +klimatyzacja")
+                } else if (autohatchback.isChecked) {
+                    wynik.setText("Twoja konfiguracja: Hatchback +klimatyzacja")
+                }
+            if (!klimatyzacja.isChecked && skorzanesiedzenia.isChecked)
+                if (autosedan.isChecked) {
+                    wynik.setText("Twoja konfiguracja: Sedan +skórzane siedzenia")
+                } else if (autosuv.isChecked) {
+                    wynik.setText("Twoja konfiguracja: SUV +skórzane siedzenia")
+                } else if (autohatchback.isChecked) {
+                    wynik.setText("Twoja konfiguracja: Hatchback +skórzane siedzenia")
+                }
+            if (!klimatyzacja.isChecked && !skorzanesiedzenia.isChecked)
+                if (autosedan.isChecked) {
+                    wynik.setText("Twoja konfiguracja: Sedan")
+                } else if (autosuv.isChecked) {
+                    wynik.setText("Twoja konfiguracja: SUV")
+                } else if (autohatchback.isChecked) {
+                    wynik.setText("Twoja konfiguracja: Hatchback")
+                }
+        }
+    }
+}
